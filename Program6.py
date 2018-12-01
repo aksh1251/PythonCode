4fb48e2986e53b10350f3023359a64d5621dfd6b
Akshaya <aksh1997ak@gmail.com>
2018-12-01 08:51:23 +0530

    Sixth Program

---
+++ Program6.py
@@ -0,0 +1,10 @@
+message = [1,9.2,"Guten Tag",26,9,14]
+print(message)
+
+message[0]=["Gute Nacht"]
+print(message[0])
+print(type(message[0]))
+
+message[5]=3.7
+print(message[5])
+print(type(message[5]))
