ae99f6fd0709fb77fa7477000addab6fd819ebec
Akshaya <aksh1997ak@gmail.com>
2018-12-01 13:47:58 +0530

    SumFloat

---
+++ SummationFloat.py
@@ -0,0 +1,9 @@
+number1 = float(input("Enter number 1: "))
+number2 = float(input("Enter number 2: "))
+
+summation = number1 + number2
+
+print(summation)
+print(type(number1))
+print(type(number2))
+print(type(summation))
