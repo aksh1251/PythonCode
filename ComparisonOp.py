1ed8563b732b09756901d279c3d2abe72e77941b
Akshaya <aksh1997ak@gmail.com>
2018-12-01 15:01:45 +0530

    ComparisonOp

---
+++ ComparisonOp.py
@@ -0,0 +1,11 @@
+a = int(input("enter number 1: "))
+b = int(input("enter number 2: "))
+
+print("when a=",a,",b=",b)
+
+print("a==b returns", a==b)
+print("a!=b returns", a!=b)
+print("a>b returns", a > b)
+print("a<b returns", a < b)
+print("a>=b returns", a >= b)
+print("a<=b returns", a <= b)
