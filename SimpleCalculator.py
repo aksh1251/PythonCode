2d54536ee62c062b732976df4546db6628322d5c
Akshaya <aksh1997ak@gmail.com>
2018-12-01 10:58:27 +0530

    Simple Calculator

---
+++ SimpleCalculator.py
@@ -0,0 +1,12 @@
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print("Sum: ",addition)
+print("Difference: ", subtraction)
+print("Product: ", multiplication)
+print("Quotient: ", division)
