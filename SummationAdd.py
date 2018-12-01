a424716b728ddf2ae94145d7ccfe34c37a6e9833
Akshaya <aksh1997ak@gmail.com>
2018-12-01 13:43:35 +0530

    SumAdd

---
+++ SummationAdd.py
@@ -0,0 +1,9 @@
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+summation = number1 + number2
+
+print(summation)
+print(type(number1))
+print(type(number2))
+print(type(summation))
