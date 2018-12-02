e38b37ce584159c4b9e1b210e6571fa0e169d010
Akshaya <aksh1997ak@gmail.com>
2018-12-02 15:49:56 +0530

    Membership

---
+++ MembershipOp.py
@@ -0,0 +1,11 @@
+c = [4,3]
+a = [1, 2, 3]
+
+result1 = (a in c)
+result2 = (a not in c)
+result3 = (c in a)
+
+
+print("a in c =", result1)
+print("a not in c =", result2)
+print("c in a =", result3)
